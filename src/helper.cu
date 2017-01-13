21b3faae91525734fe3210c526e8d91bbe95da56
waltsims <walter.a.simson@gmail.com>
2017-01-13 18:14:46 +0100

    initial commit for dynamic problem sizing

---
+++ src/helper.cu
@@ -0,0 +1,14 @@
+#include "helper.cuh"
+
+extern "C" bool isPow2(unsigned int x) { return ((x & (x - 1)) == 0); }
+
+// Finds the next largest power 2 number
+extern "C" size_t nextPow2(size_t x) {
+  --x;
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+  return ++x;
+}
