f513bb0e0a32eb30f536d88d8966345e8522f2bb
waltsims <walter.a.simson@gmail.com>
2017-02-01 15:15:34 +0100

    added expected value plot to output

--- src/plot.cpp
+++ src/plot.cpp
@@ -6,57 +6,94 @@
 
 // this is the cumsum devided by number of relative paths (insitu)
 template <typename T>
-void boundary2ExpectancyValue(T *vals, int paths) {
-
-  for (int i = 1; i < paths; i++) {
+void cumsum(T *vals, int paths) {
+  for (int i = 1; i < paths; i++)
     vals[i] += vals[i - 1];
-    vals[i - 1] /= i;
+}
+
+template <typename T>
+void calcExpectedValue(T *vals, int paths) {
+  cumsum(vals, paths);
+  for (int i = 0; i < paths; i++) {
+    vals[i] /= i + 1;
   }
-  vals[paths - 1] /= paths;
 }
 // callculate the relative error (insitu)
 template <typename T>
-void getRelativeError(T *data, int paths, T end) {
-
+void calcRelativeError(T *data, int paths, T exactSolution) {
   for (int i = 0; i < paths; i++) {
-    data[i] = fabs((data[i] - end) / end);
+    data[i] = fabs((data[i] - exactSolution) / exactSolution);
   }
+}
+void outputHeader(std::ofstream &file) {
+  file << "paths,"
+       << "relativeError,"
+       << "expectedValue,"
+       << "value" << std::endl;
 }
 
 template <typename T>
-void outputConvergence(const char *filename, T *data, T *values, int paths) {
+void logOutputData(const char *filename, T *relError, T *values,
+                   T *expectedValue, int paths) {
   // TODO impliment for run numbers greater than MAX_BLOCKS
-  std::cout << "writing file to: " << filename << std::endl;
-  std::ofstream file(filename);
-  file << "paths\t"
-       << "relative error\t"
-       << "value\t" << std::endl;
   // only export every 1.25^n val reduce file size
-  // TODO remove logarithmic recution to seperate functionk
-  int points = (int)ceil(log(paths) / log(1.25));
 
+  printInfo("writing to file:");
+  std::cout << "\t" << filename << std::endl;
+  std::ofstream file(filename);
+  outputHeader(file);
+  // TODO remove logarithmic recution to seperate function
+  int points = (int)ceil(log(paths) / log(1.25));
+  int last = 0;
+  int currentPoint;
   for (int i = 2; i < points; i++) {
-    file << (int)floor(pow(1.25, i)) << "\t" << data[(int)floor(pow(1.25, i))]
-         << "\t" << values[(int)floor(pow(1.25, i))] << std::endl;
+    currentPoint = (int)floor(pow(1.25, i));
+    if (last != currentPoint) {
+      file << currentPoint << "," << relError[currentPoint] << ","
+           << expectedValue[currentPoint] << "," << values[currentPoint]
+           << std::endl;
+      last = currentPoint;
+    }
   }
   file.close();
 }
+
+template <typename T>
+void linearOutputConvergence(const char *filename, T *relError, T *values,
+                             T *expectedValue, int paths) {
+  // TODO impliment for run numbers greater than MAX_BLOCKS
+  std::cout << "writing file to: " << filename << std::endl;
+
+  std::ofstream file(filename);
+  outputHeader(file);
+  // TODO remove logarithmic recution to seperate functionk
+  for (int currentPoint = 0; currentPoint < paths; currentPoint++) {
+    file << currentPoint << "\t" << relError[currentPoint] << "\t"
+         << expectedValue[currentPoint] << "\t" << values[currentPoint]
+         << std::endl;
+  }
+  file.close();
+}
+
 void plot(double *h_paths, Parameters &p) {
   typedef double T;
   // T end = vals[paths - 1]; // use last value to test convergence
-  T end = 0.29468541312605526226;
+  T exactSolution = 0.29468541312605526226;
 
   T *data = (T *)malloc(p.wos.totalPaths * sizeof(double));
+  T *expectedValue = (T *)malloc(p.wos.totalPaths * sizeof(double));
   std::memcpy(data, h_paths, p.wos.totalPaths * sizeof(double));
 
   // std::cout << "data\n" << std::endl;
   // for (int i = 0; i < p.wos.totalPaths; i++)
   //   std::cout << data[i] << std::endl;
 
-  printf("exporting convergences data\n");
-  boundary2ExpectancyValue(data, p.wos.totalPaths);
-  getRelativeError(data, p.wos.totalPaths, end);
-  outputConvergence("docs/data/cuWos_convergence.dat", data, h_paths,
-                    p.wos.totalPaths);
+  printInfo("exporting simulation data");
+  calcExpectedValue(data, p.wos.totalPaths);
+  std::memcpy(expectedValue, data, p.wos.totalPaths * sizeof(double));
+  calcRelativeError(data, p.wos.totalPaths, exactSolution);
+  logOutputData("docs/data/cuWos_data.csv", data, h_paths, expectedValue,
+                p.wos.totalPaths);
   free(data);
+  free(expectedValue);
 }
--- src/plot.h
+++ src/plot.h
@@ -3,13 +3,21 @@
 class Parameters; // forward declaration
 // this is the cumsum devided by number of relative paths (insitu)
 template <typename T>
-void boundary2ExpectancyValue(T *vals, int paths);
+void calcExpectedValue(T *vals, int paths);
 
 // callculate the relative error (insitu)
 template <typename T>
-void getRelativeError(T *data, int paths, T end);
+void calcRelativeError(T *data, int paths, T end);
 
 template <typename T>
-void outputConvergence(const char *filename, T *vals, T *values, int paths);
+void cumsum(T *vals, int paths);
+
+void outputHeader(std::ofstream &file);
+
+template <typename T>
+void logOutputData(const char *filename, T *vals, T *values, int paths);
+
+template <typename T>
+void linearOutputData(const char *filename, T *vals, T *values, int paths);
 
 void plot(double *h_paths, Parameters &p);
